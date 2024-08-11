c7265b8a415ead5eccf829f4fdde0be6927ad4b7
japolinar <japolinar_1@hotmail.com>
2024-08-11 14:39:11 -0400

    ruta imagen 02

--- src/data/db.ts
+++ src/data/db.ts
@@ -12,66 +12,66 @@ export const menuItems: MenuItem[] = [
       "id": 2,
       "name": "Pizza a la Leña Mediana",
       "price": 50,
-      "imagen": "./src/assets/img/pizzas.jpg"
+      "imagen": "img/pizzas.jpg"
     },
     {
       "id": 3,
       "name": "Rebanada de Pay de Limón",
       "price": 30,
-      "imagen": "./src/assets/img/pastel.jpg"
+      "imagen": "img/pastel.jpg"
     },
     {
       "id": 4,
       "name": "Rebanada de Pastel de Chocolate",
       "price": 30,
-      "imagen": "./src/assets/img/pastel_chocolate.jpg"
+      "imagen": "img/pastel_chocolate.jpg"
     },
     {
       "id": 5,
       "name": "Jugo de Naranja",
       "price": 15,
-      "imagen": "./src/assets/img/drinks_orange.svg"
+      "imagen": "img/drinks_orange.svg"
     },
     {
       "id": 6,
       "name": "Pizza a la Leña Grande",
       "price": 70,
-      "imagen": "./src/assets/img/pizza_02.svg"
+      "imagen": "img/pizza_02.svg"
     },
     {
       "id": 7,
       "name": "Rib Eye 800g",
       "price": 100,
-      "imagen": "./src/assets/img/drinks_orange.svg"
+      "imagen": "img/drinks_orange.svg"
     },
     {
       "id": 8,
       "name": "Jugo de Naranja",
       "price": 15,
-      "imagen": "./src/assets/img/drinks_orange.svg"
+      "imagen": "img/drinks_orange.svg"
     },
     {
       "id": 9,
       "name": "Tequila",
       "price": 40,
-      "imagen": "./src/assets/img/tequila.svg"
+      "imagen": "img/tequila.svg"
     },
     {
       "id": 10,
       "name": "Rebanada de Pay de Queso",
       "price": 30,
-      "imagen": "./src/assets/img/pastel.jpg"
+      "imagen": "img/pastel.jpg"
     },
     {
       "id": 11,
       "name": "Café Americano",
       "price": 20,
-      "imagen": "./src/assets/img/cafe.jpg"
+      "imagen": "img/cafe.jpg"
     },
     {
       "id": 12,
       "name": "Café Capuchino",
       "price": 40,
-      "imagen": "./src/assets/img/cafe_capuchino.jpg"
+      "imagen": "img/cafe_capuchino.jpg"
     }
 ]
